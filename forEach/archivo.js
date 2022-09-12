62f3c509bb64bcc891cad276f8d892608d59ace4
MarceloYudis <marceloyudisortiz@gmail.com>
2022-09-12 19:10:56 -0400

    arreglo de checkbox

--- forEach/archivo.js
+++ forEach/archivo.js
@@ -8,3 +8,14 @@ const parrafoHtml = document.getElementById('main');
 comida.forEach( producto => {
     parrafoHtml.innerHTML += `<li> ${producto.nombre} </li>`;
 })
+
+const actividades = [
+    {tarea: 'Lavar la ropa'},
+    {tarea: 'Comprar la despensa'}
+]
+
+const listadoHtml = document.getElementById('listaTareas');
+
+let completasion = actividades.forEach( unaActividad => {
+    listadoHtml.innerHTML += ` <p><input type="checkbox">${unaActividad.tarea}  </p>`
+})
