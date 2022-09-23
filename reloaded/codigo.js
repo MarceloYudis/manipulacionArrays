0e7971d7054620b1f6b3bd5d717805be0cd66ff8
MarceloYudis <marceloyudisortiz@gmail.com>
2022-09-23 19:18:50 -0400

    uso del map para evitar mutar el array original mediante spread operator

---
+++ reloaded/codigo.js
@@ -0,0 +1,44 @@
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ]
+
+  console.log("ordenes " + ordenes)
+  const ordenesTotales = orders.map( item => { item.total })
+  console.log("ordenes totales " + ordenesTotales)
+
+ /* const ordenesNuevoAtrib = orders.map( item => {
+    item.tax = .19;
+    return item
+  }) */
+
+
+
+
+  const ordenesNuevoAtrib2 = orders.map( item => {
+
+    return {
+        ...item,
+        tax: .19
+    }
+  })
+  console.log(ordenesNuevoAtrib2);
+  console.log(orders);
