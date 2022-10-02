d80bfba654f29e5e85aef86c3b87888fa19a5c72
MarceloYudis <marceloyudisortiz@gmail.com>
2022-10-02 18:05:39 -0300

    concatenaciones con .concat()

---
+++ concat/concat.js
@@ -0,0 +1,6 @@
+const garen = ["motor", "alarmas","camaras", "electrificadores"]
+const hik = ["camaras IP","control de acceso","video porteros de edificios"]
+
+const marcas = garen.concat(hik)
+
+console.log(marcas)
